54a1e3538ba3f25f344a91cc05e33eb36d228ea9
NexusUA <pbogomolny93@gmal.com>
2022-09-30 12:26:58 +0200

    Initial Commit

---
+++ owow/owowApp.swift
@@ -0,0 +1,17 @@
+//
+//  owowApp.swift
+//  owow
+//
+//  Created by Pavel Bohomolnyi on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct owowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
