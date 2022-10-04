eb8539c17de15594d3160a510c24e2845499db9c
NexusUA <pbogomolny93@gmal.com>
2022-10-04 13:23:57 +0200

    - update DetailView

--- owow/Views/DetailView.swift
+++ owow/Views/DetailView.swift
@@ -8,36 +8,37 @@
 import SwiftUI
 
 struct DetailView: View {
-        
+    
     var character: Result
     
     var body: some View {
         
-        VStack {
+        ScrollView {
             
-            Text(character.name)
-                .font(.title)
-                .bold()
-                .padding()
-            
-            AsyncImage(url: URL(string: character.image)) { image in
-                image.resizable()
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 150, height: 150)
-            .cornerRadius(5)
-        
-
-            HStack {
-                Image(systemName: "location")
+            VStack {
                 
-                Text("Location info:")
-            }
-            .padding()
-            
-            Text(character.location.name)
+                AsyncImage(url: URL(string: character.image)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .scaledToFill()
+                
+                Text(character.name)
+                    .font(.title)
+                    .bold()
+                    .padding()
+                
+                HStack {
+                    Image(systemName: "location")
+                    
+                    Text("Location info:")
+                }
                 .padding()
+                
+                Text(character.location.name)
+                    .padding()
+            }
         }
     }
 }
